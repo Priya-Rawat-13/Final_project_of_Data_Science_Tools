0b75be991c580715cab9f9c6a542a07e7f31a94e
Priya-Rawat-13 <132638108+Priya-Rawat-13@users.noreply.github.com>
2023-06-27 21:55:35 +0530

    this is the python file

---
+++ priya_Data_Science_Ecosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Objectives:
+# 
+# + popular languages that Data Scientists use
+# + commonly used libraries used by Data Scientists
+# + Data Science tools
+
+# Some of the popular languages that Data Scientists use are:
+# 
+#     1. Python
+#     2. R
+#     3. Scala
+#     4. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     
+#     1. Pandas
+#     2. Numpy
+#     3. Matplotlib
+#     4.TensorFlow
+
+# |Data Science Tools|
+# |------------------|
+# |MySQL|
+# |postgreSQL|
+# |MongoDB|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+(3*4)+5 #This a simple arithmetic expression to mutiply then add integers
+
+
+# In[4]:
+
+
+minutes=200
+hours=minutes/60 #This will convert 200 minutes to hours by diving by 60
+hours
+
+
+# ## Author
+# 
+# Priya Rawat
+
+# In[ ]:
+
+
+
+
